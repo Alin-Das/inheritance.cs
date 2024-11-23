9ba2df8db36ca5c7d86437cce4428e2088909fbd
Alin Das <dasalin44@gmail.com>
2024-11-24 01:07:54 +0600

    Add files via upload

---
+++ LabClass 4 (22-46379-1)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Vehicle
+{
+    public string VehicleName;
+    public string VehicleId;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($" Vehicle Name : {VehicleName}, VehicleId: {VehicleId}");
+    }
+    public void Status()
+    {
+        Console.WriteLine("--- THIS A VEHICLE ---");
+    }
+}
+class MotorBike : Vehicle { }
+class Truck : Vehicle { }
+class Car : Vehicle { }
+class Saloon : Car { }
+class MPV : Car { }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle motorbike = new MotorBike();
+        motorbike.VehicleName = "TESLA";
+        motorbike.VehicleId = "KP-002";
+        motorbike.ShowInfo();
+        motorbike.Status();
+
+        Vehicle truck = new Truck();
+        truck.VehicleName = "TOYOTA";
+        truck.VehicleId = "RWS-0365";
+        truck.ShowInfo();
+        truck.Status();
+
+        Vehicle mpv = new MPV();
+        mpv.VehicleName = "KOENENSIGG-JESCO";
+        mpv.VehicleId = "AAA-008";
+        mpv.ShowInfo();
+        mpv.Status();
+
+    }
+        
+}
